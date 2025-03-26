4ddfdd758f802d25eb2a6c341a8da878d610867f
Jinchu Li <jinchuli2002@gmail.com>
2025-03-26 08:26:08 -0400

    RAFT loss

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -635,7 +635,6 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
 
     def _loss_step(self, batch: Dict[str, torch.Tensor]) -> torch.Tensor:
         # CRITICAL CHANGE HERE:
-        print(batch)
         labels = batch.pop("numeric_label")  # <- This is numeric truth (0-999)
 
         with self.activations_handling_ctx:
