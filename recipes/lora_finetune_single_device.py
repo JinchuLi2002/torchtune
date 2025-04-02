f17734e310e95a4ffde1c283c562784e15753fa0
Jinchu Li <jinchuli2002@gmail.com>
2025-04-02 05:10:16 -0400

    RAFT loss customized

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -572,10 +572,9 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
             dataset=ds,
             shuffle=shuffle,
             batch_size=batch_size,
-            collate_fn=(collate_fn if not packed else padded_collate_packed),
+            collate_fn=collate_fn,
             drop_last=True,
         )
-
         if dataloader_state_dict is not None:
             dataloader.load_state_dict(dataloader_state_dict)
             # B/c we currently only save at epoch boundaries, if we cut the previous epoch short
