03da066202554ae5465b238461144b2a384cabbd
Jinchu Li <jinchuli2002@gmail.com>
2025-04-06 19:58:26 -0400

    RAFT loss + learnable weight

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -717,7 +717,7 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
         # Decode top tokens
         top_tokens_str = [self._tokenizer.decode([tid.item()]).strip() for tid in top_token_ids]
 
-        loss = mse # + 10000 * entropy
+        loss = mse + 1000000 * (non_numeric_penalty ** 2)
         print(f"[DEBUG] preds_numeric: {preds_numeric.tolist()} | target: {numeric_labels.tolist()} | mse: {mse.item():.4f}")
         print(f"[DEBUG] top token ids: {top_token_ids.tolist()} | decoded: {top_tokens_str}")
         print(f'[DEBUG] numeric penalty: {non_numeric_penalty.item():.4f} | entropy: {entropy.item():.4f} | loss contribution: {1000000 * (non_numeric_penalty ** 2):.4f} | total loss: {loss.item():.4f}')
