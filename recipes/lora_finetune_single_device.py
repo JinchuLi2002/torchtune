70a2b338daa52559cfddb9f169cccb97e21b0357
Jinchu Li <jinchuli2002@gmail.com>
2025-04-05 23:42:46 -0400

    RAFT loss + learnable weight

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -690,7 +690,12 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
 
         full_probs = F.softmax(logits[:, -1, :], dim=-1)
         numeric_probs = full_probs[:, numeric_token_ids]  # slice out numeric tokens
-        preds_numeric = torch.sum(numeric_probs * numeric_values, dim=-1)
+        # preds_numeric = torch.sum(numeric_probs * numeric_values, dim=-1)
+
+        # Learnable weights: w_y for each token y
+        self.learnable_weights = torch.nn.Parameter(torch.arange(1000, dtype=torch.float32, device=self._device))
+
+        preds_numeric = torch.sum(numeric_probs * self.learnable_weights, dim=-1)
 
         # MSE Loss
         mse = F.mse_loss(preds_numeric, numeric_labels.float())
