f161a18e2c9f9eef4ef4de4b5513e40ff3a3e177
Jinchu Li <jinchuli2002@gmail.com>
2025-04-11 13:11:22 -0400

    mae w CE entropy

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -739,8 +739,8 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
         mae_weight = 1.0
         numeric_ce_loss = F.cross_entropy(
             logits[:, -1, numeric_token_ids],
-            numeric_labels.long(),
-            label_smoothing=0.1  # important!
+            numeric_label_indices, 
+            label_smoothing=0.1
         )
 
         loss = mae_weight * mae + ce_weight * numeric_ce_loss
