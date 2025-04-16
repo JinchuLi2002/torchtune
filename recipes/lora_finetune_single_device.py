f9a7332c98476e3ffcc44a9caf58d23d15724fc8
Jinchu Li <jinchuli2002@gmail.com>
2025-04-15 20:06:05 -0400

    mae w CE entropy

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -764,7 +764,17 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
             numeric_label_indices,
             label_smoothing=0.1
         )
-        loss = 0.5* kl_loss + mae
+        if current_epoch < 5:
+            ce_weight = 10.0 # or is it 2
+        elif current_epoch < 10:
+            ce_weight = 0.8
+        else:
+            ce_weight = 0.2
+
+        mae_weight = 1.0
+
+        # loss = mae_weight * mae + ce_weight * numeric_ce_loss
+        loss = mae_weight * mae + ce_weight * kl_loss
 
 
         #loss = mae #+ 1000000 * (non_numeric_penalty ** 2)
