664fc7fd8d59e42129a5452bb57725e5f24e53ba
Jinchu Li <jinchuli2002@gmail.com>
2025-04-23 12:29:22 -0400

    mae w CE entropy

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -773,7 +773,7 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
         mae_weight = 1.0
 
         #loss = mae_weight * mae + ce_weight * numeric_ce_loss #ce_weight * kl_loss 
-        loss = mae
+        loss = numeric_ce_loss
 
         #loss = mae #+ 1000000 * (non_numeric_penalty ** 2)
         #print(f"[DEBUG] preds_numeric: {preds_numeric.tolist()} | target: {numeric_labels.tolist()} | mse: {mse.item():.4f}")
